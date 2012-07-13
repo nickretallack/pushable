9c67a0e7578ae767884cfd3d3856ce77406396ab
Nick Retallack <nickretallack@gmail.com>
2012-07-13 02:02:36 -0700

    fetch a list of objects and log em

--- client.coffee
+++ client.coffee
@@ -2,5 +2,19 @@ faye = new Faye.Client '/faye'
 subscription = faye.subscribe '/foo', (message) ->
     console.log JSON.parse message
 
-subscription.callback -> console.log "subscription is now active"
+subscription.callback ->
+    console.log "subscription is now active"
 subscription.errback (error) -> console.log "Error: #{error}"
+
+all_things = {}
+
+this_frame = {}
+next_frame = {}
+
+class Thing
+    constructor: ({@size, @id}) ->
+        things[@id] = @
+
+$.get '/objects', (things) ->
+    for id, thing of things
+        new Thing thing
--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 (function() {
-  var faye, subscription;
+  var Thing, all_things, faye, next_frame, subscription, this_frame;
 
   faye = new Faye.Client('/faye');
 
@@ -13,6 +13,33 @@
 
   subscription.errback(function(error) {
     return console.log("Error: " + error);
+  });
+
+  all_things = {};
+
+  this_frame = {};
+
+  next_frame = {};
+
+  Thing = (function() {
+
+    function Thing(_arg) {
+      this.size = _arg.size, this.id = _arg.id;
+      things[this.id] = this;
+    }
+
+    return Thing;
+
+  })();
+
+  $.get('/objects', function(things) {
+    var id, thing, _results;
+    _results = [];
+    for (id in things) {
+      thing = things[id];
+      _results.push(new Thing(thing));
+    }
+    return _results;
   });
 
 }).call(this);
